b4f5994ef4a89cbf47c83e6d65336f30dfedeb77
Linus Oleander <linus.oleander@clark.de>
2023-10-17 17:39:58 +0200

    More cleanup

--- src/main.rs
+++ src/main.rs
@@ -16,14 +16,21 @@ fn app_main() {
   esp_idf_sys::link_patches();
   esp_idf_svc::log::EspLogger::initialize_default();
 
+  info!("Starting clickbot loop");
   let mut keyboard = Keyboard::new();
 
-  info!("Starting clickbot loop");
 
+  info!("Running tests 10 times with 5 second delay");
+  for _ in 0..10 {
+      keyboard.send_media_key(CONSUMER_CONTROL_CONFIGURATION);
+      Ets::delay_ms(5000);
+  }
+
+  info!("Starting main clickbot loop");
   loop {
     if keyboard.connected() {
       info!("Sending awake command");
-      keyboard.send_media_key(EMAIL_READER);
+      keyboard.send_media_key(CONSUMER_CONTROL_CONFIGURATION);
       Ets::delay_ms(5 * 1000 * 60); // 5 minutes
     } else {
       info!("Waiting for keyboard to connect");
